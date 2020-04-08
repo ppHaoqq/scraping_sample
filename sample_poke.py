30b151f7756c34addd5921fecc87fa5e0c4cbf3d
daiki <daiki.nitta888@gmail.com>
2020-04-08 17:16:11 +0900

    udemy = sample8 scrapy単一ファイルに簡単な練習:sample9 ポケDB完成！:sample_poke.py

--- sample_poke.py
+++ sample_poke.py
@@ -1,46 +1,57 @@
+
 import time
+import re
 import requests
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup as bs
 from pymongo import MongoClient
 
-client = MongoClient('localhost', 27017)
-db = client.pokemon
-collection = db.pokedb
-collection.delete_many({})
-
 main_url = 'https://yakkun.com/swsh/zukan/'
+
+
+def extract_key(url):
+    m = re.search(r'([^/])+$', url)
+    _key = m.group(0)
+    return _key
+
+#クライアント取得
+client = MongoClient('localhost', 27017)
+#データベース取得（pokemon）
+db = client.pokemon
+#テーブル取得(pokedb)
+collection = db.pokedb
+#collection.delete_many({})=>練習用に都度削除するための構文
+collection.create_index('key', unique=True)
 
 r = requests.get(main_url)
 soup = bs(r.content, 'html.parser')
-
 data = soup.select('#contents > div.pokemon_list_box > ul.pokemon_list > li > a')
 
-urls = []
-count = 0
+#count = 0
 for _url in data:
-    if count == 10:
-        break
-    else:
-        time.sleep(1)
-        urls.append(urljoin(main_url, _url.get('href')))
-        count += 1
-
-for url in urls:
-    r2 = requests.get(url)
-    soup2 = bs(r2.content, 'html.parser')
-
-    name = soup2.select('.head > th[colspan="2"]')[0].text
-    weight = soup2.select('#base_anchor > table > tr:nth-child(7) > td:nth-child(2) > ul > li:nth-child(1)')[0].text
-    h = int(soup2.select('#stats_anchor > table > tr:nth-child(2) > td.left')[0].text)
-    a = int(soup2.select('#stats_anchor > table > tr:nth-child(3) > td.left')[0].text)
-    b = int(soup2.select('#stats_anchor > table > tr:nth-child(4) > td.left')[0].text)
-    c = int(soup2.select('#stats_anchor > table > tr:nth-child(5) > td.left')[0].text)
-    d = int(soup2.select('#stats_anchor > table > tr:nth-child(6) > td.left')[0].text)
-    s = int(soup2.select('#stats_anchor > table > tr:nth-child(7) > td.left')[0].text)
-    poke = {'name': name, 'weight': weight, 'h': h, 'a': a, 'b': b, 'c': c, 'd': d, 's': s}
-    collection.insert_one(poke)
-
-cursor = collection.find()
-for link in cursor:
-    print(link['name'])
+        url2 = urljoin(main_url, _url.get('href'))
+        key = extract_key(url2)
+        exist_poke = collection.find_one({'key': key})
+        if not exist_poke:
+            time.sleep(2)
+            r2 = requests.get(url2)
+            soup2 = bs(r2.content, 'html.parser')
+            #soup5までいらんクラスの削除、勝手にsoup2も変更される
+            soup3 = soup2.find_all(class_='needless')
+            for soup4 in soup3:
+                soup5 = soup4.decompose()
+            poke = {
+                'key': extract_key(url2),
+                'name': soup2.select('.head > th[colspan="2"]')[0].text,
+                'weight': soup2.select('.center > td > ul > li')[0].text,
+                'h': int(soup2.select('#stats_anchor > table > tr:nth-child(2) > td.left')[0].text),
+                'a': int(soup2.select('#stats_anchor > table > tr:nth-child(3) > td.left')[0].text),
+                'b': int(soup2.select('#stats_anchor > table > tr:nth-child(4) > td.left')[0].text),
+                'c': int(soup2.select('#stats_anchor > table > tr:nth-child(5) > td.left')[0].text),
+                'd': int(soup2.select('#stats_anchor > table > tr:nth-child(6) > td.left')[0].text),
+                's': int(soup2.select('#stats_anchor > table > tr:nth-child(7) > td.left')[0].text)
+            }
+            collection.insert_one(poke)
+            #count += 1
+        else:
+            print(collection.find_one({'key': key}))
